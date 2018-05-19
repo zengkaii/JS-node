10ed1e30040051a71ab6f85315ff468dd386bba8
zengkai <1965599425@qq.com>
2018-05-19 12:47:23 +0800

    添加了es6学习

---
+++ es6/map/index.js
@@ -0,0 +1,17 @@
+// const dogs = new Map();
+// dogs.set('Snickers',3);
+// dogs.set('Sunny',2);
+// dogs.set('hugo',10);
+
+// dogs.forEach((val,key) => console.log(
+//     val,key));
+//     // 可以迭代的都可以使用for each
+// for(const [key,val] of dogs) {
+//     console.log(key,val);
+// }
+
+let a = 1;
+let b = 2;
+[a , b]=[b , a];
+console.log(a ,b );
+
